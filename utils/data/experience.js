9eaa4a68de583691c9550184492285966dd8558e
Meet Patel <113357985+Meet0000@users.noreply.github.com>
2025-03-30 20:03:31 -0400

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,15 +1,20 @@
 export const experiences = [
   {
     id: 1,
-    title: "Self Employed",
-    company: "Coding and building website and software.",
-    duration: "(Aug 2023 - Present)"
+    title: "Helpdesk Support Analyst-D365",
+    company: "Total Power Limited",
+    duration: "(Jan 2025 - Present)"
   },
   {
     id: 2,
+    title: "Self Employed",
+    company: "Coding and building website and software.",
+    duration: "(Aug 2023 - Sep 2024)"
+  },
+  {
+    id: 3,
     title: 'Jr. Front-end Software developer',
     company: "AGM Etech Pvt Ltd.",
     duration: "(Sep 2021 - Jun 2023)"
   }
-
 ]
