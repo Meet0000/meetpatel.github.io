70c3ed67526d60066ffe13ec2d1fd610464bda6b
Meet Patel <113357985+Meet0000@users.noreply.github.com>
2025-10-24 19:51:57 -0400

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,15 +1,15 @@
 export const experiences = [
   {
-    id: 2,
-    title: "Helpdesk Support Analyst-D365",
-    company: "Total Power Limited",
-    duration: "(Jan 2025 - Apr 2025)"
-  },
-  {
     id: 1,
     title: "IT Support Analyst",
     company: "Total Power Limited",
     duration: "(May 2025 - Present)"
+  },
+    {
+    id: 2,
+    title: "Helpdesk Support Analyst-D365",
+    company: "Total Power Limited",
+    duration: "(Jan 2025 - Apr 2025)"
   },
   {
     id: 3,
