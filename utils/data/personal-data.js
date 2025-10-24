242b66a8c250963e6511eb9017d3f3911be78182
Meet Patel <113357985+Meet0000@users.noreply.github.com>
2025-10-24 19:39:24 -0400

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,9 +1,9 @@
 export const personalData = {
   name: "MEET PATEL",
   profile: '/profile.jpg',
-  designation: "Full-Stack Software Developer",
-  description: "My name is MEET PATEL. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
-  email: 'meetpately@gmail.com',
+  designation: "IT Support Analyst & Software Developer",
+  description: "My name is Meet Patel, a passionate programmer and IT Support Analyst with strong problem-solving skills and a self-learning attitude. I have hands-on experience in managing IT systems, troubleshooting technical issues, and ensuring system security. With expertise in JavaScript and web development, I enjoy creating efficient solutions and bringing innovation to both development and IT operations. I’m always eager to take on opportunities that match my skills and interests.",
+  email: 'patelmeetkadi@gmail.com',
   phone: '+1 437-766-3098',
   address: '56 Gower Cres,Brampton,L6R0Y1,CA ',
   github: 'https://github.com/Meet0000',
@@ -13,6 +13,6 @@ export const personalData = {
   stackOverflow: 'https://stackoverflow.com/meet',
   leetcode: "https://leetcode.com/meet9574/",
   devUsername: "meet9574",
-  resume: "https://drive.google.com/file/d/1qK5IT1vD75fEoppCSZ3BTQjOSYLX6PQm/view?usp=sharing",
+  resume: "https://drive.google.com/file/d/1QBcYsB4gVDzLJaCxykmpsJWLxOMXWpAR/view?usp=sharing",
   footerlogo: '/heart.png'
 }
